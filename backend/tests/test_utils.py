2873b3e0a9d36c5f186fd9df401e83f533ea9f8e
zjowowen <zjowowen@outlook.com>
2024-11-01 15:19:51 +0800

    polish code

--- backend/tests/test_utils.py
+++ backend/tests/test_utils.py
@@ -1,71 +1,74 @@
 import os
 import librosa
-import soundfile as sf
-import unittest
 import sys
 
-sys.path.append('..')
+import numpy as np
+
+sys.path.append("..")
 from utils import adjust_volume, change_speed, shift_pitch
 
-# Unit tests
-class TestAudioProcessing(unittest.TestCase):
-    def test_adjust_volume(self):
-        if not os.path.exists(os.path.join(os.path.dirname(__file__), '../ref_audio/ref_wav/test')):
-            os.makedirs(os.path.join(os.path.dirname(__file__), '../ref_audio/ref_wav/test'))
-        input_path = os.path.join(os.path.dirname(__file__), '../ref_audio/ref_wav/ref_audio_default.wav')
-        output_path_1 = os.path.join(os.path.dirname(__file__), '../ref_audio/ref_wav/test/adjusted_volume_1.wav')
-        output_path_2 = os.path.join(os.path.dirname(__file__), '../ref_audio/ref_wav/test/adjusted_volume_2.wav')
+if __name__ == "__main__":
 
-        adjust_volume(input_path, output_path_1, volume_factor = 0.5)
-        adjust_volume(input_path, output_path_2, volume_factor = 2.0)
-        
-        # Load the output file and check if it's been modified
-        waveform, sr = librosa.load(input_path)
-        waveform_1, sr = librosa.load(output_path_1)
-        waveform_2, sr = librosa.load(output_path_2)
-        self.assertIsNotNone(waveform)
-        self.assertIsNotNone(waveform_1)
-        self.assertIsNotNone(waveform_2)
+    def test_adjust_volume():
+        if not os.path.exists(
+            os.path.join(os.path.dirname(__file__), "../ref_audio/ref_wav/test")
+        ):
+            os.makedirs(
+                os.path.join(os.path.dirname(__file__), "../ref_audio/ref_wav/test")
+            )
+        input_path = os.path.join(
+            os.path.dirname(__file__), "../ref_audio/ref_wav/ref_audio_default.wav"
+        )
+
+        audio, sample_rate = librosa.load(input_path)
+        audio_1 = adjust_volume(audio, volume_factor=0.5)
+        audio_2 = adjust_volume(audio, volume_factor=2.0)
 
         # compute power of the waveform
-        power = waveform.pow(2).sum().item()
-        power_1 = waveform_1.pow(2).sum().item()
-        power_2 = waveform_2.pow(2).sum().item()
+        power = (audio**2).sum().item()
+        power_1 = (audio_1**2).sum().item()
+        power_2 = (audio_2**2).sum().item()
 
         # check if the power has been adjusted
-        self.assertGreater(power, power_1)
-        self.assertGreater(power_2, power)
+        assert power_1 < power
+        assert power_2 > power
 
-    def test_change_speed(self):
-        if not os.path.exists(os.path.join(os.path.dirname(__file__), '../ref_audio/ref_wav/test')):
-            os.makedirs(os.path.join(os.path.dirname(__file__), '../ref_audio/ref_wav/test'))
-        input_path = os.path.join(os.path.dirname(__file__), '../ref_audio/ref_wav/ref_audio_default.wav')
-        output_path_1 = os.path.join(os.path.dirname(__file__), '../ref_audio/ref_wav/test/speed_changed_1.wav')
-        output_path_2 = os.path.join(os.path.dirname(__file__), '../ref_audio/ref_wav/test/speed_changed_2.wav')
+    def test_change_speed():
+        if not os.path.exists(
+            os.path.join(os.path.dirname(__file__), "../ref_audio/ref_wav/test")
+        ):
+            os.makedirs(
+                os.path.join(os.path.dirname(__file__), "../ref_audio/ref_wav/test")
+            )
+        input_path = os.path.join(
+            os.path.dirname(__file__), "../ref_audio/ref_wav/ref_audio_default.wav"
+        )
 
-        change_speed(input_path, output_path_1, speed = 0.5)
-        change_speed(input_path, output_path_2, speed = 2.0)
-        
-        # Load the output file and check if it's been modified
-        y, sr = librosa.load(input_path)
-        y_1, sr = librosa.load(output_path_1)
-        y_2, sr = librosa.load(output_path_2)
-        self.assertIsNotNone(y)
-        self.assertIsNotNone(y_1)
-        self.assertIsNotNone(y_2)
+        audio, sample_rate = librosa.load(input_path)
+        audio_1 = change_speed(audio, speed=0.5)
+        audio_2 = change_speed(audio, speed=2.0)
+
         # check if the length of the audio has been adjusted
-        self.assertGreater(len(y_1), len(y))
-        self.assertGreater(len(y), len(y_2))
-    
+        assert len(audio) < len(audio_1)
+        assert len(audio_2) < len(audio)
 
-    def test_shift_pitch(self):
-        if not os.path.exists(os.path.join(os.path.dirname(__file__), '../ref_audio/ref_wav/test')):
-            os.makedirs(os.path.join(os.path.dirname(__file__), '../ref_audio/ref_wav/test'))
-        input_path = os.path.join(os.path.dirname(__file__), '../ref_audio/ref_wav/ref_audio_default.wav')
-        output_path = os.path.join(os.path.dirname(__file__), '../ref_audio/ref_wav/test/pitch_shifted.wav')
-        n_steps = 2
-        shift_pitch(input_path, output_path, n_steps)
-        
-        # Load the output file and check if it's been modified
-        y_shifted, sr = librosa.load(output_path)
-        self.assertIsNotNone(y_shifted)
+    def test_shift_pitch():
+        if not os.path.exists(
+            os.path.join(os.path.dirname(__file__), "../ref_audio/ref_wav/test")
+        ):
+            os.makedirs(
+                os.path.join(os.path.dirname(__file__), "../ref_audio/ref_wav/test")
+            )
+        input_path = os.path.join(
+            os.path.dirname(__file__), "../ref_audio/ref_wav/ref_audio_default.wav"
+        )
+
+        n_steps = [-12, -6, 0, 6, 12]
+        audio, sample_rate = librosa.load(input_path)
+        for n in n_steps:
+            audio_shifted = shift_pitch(audio, sample_rate, n_steps=n)
+            assert audio_shifted is not None
+
+    test_adjust_volume()
+    test_change_speed()
+    test_shift_pitch()
--- backend/utils.py
+++ backend/utils.py
@@ -1,39 +1,32 @@
-import torchaudio
 import librosa
-import soundfile as sf
+import numpy as np
 
 
 # Function to adjust volume
-def adjust_volume(input_path: str, output_path: str, volume_factor: float):
+def adjust_volume(audio, volume_factor: float):
     """
     Adjust the volume of an audio file by a given factor.
     """
-    waveform, sample_rate = torchaudio.load(input_path)
-    adjusted_waveform = waveform * volume_factor
-    adjusted_waveform = adjusted_waveform.clamp(min=-1.0, max=1.0)
-    torchaudio.save(output_path, adjusted_waveform, sample_rate)
+    adjusted_audio = audio * volume_factor
+    adjusted_audio = np.clip(adjusted_audio, -1.0, 1.0)
+    return adjusted_audio
 
 
 # Function to change speed
-def change_speed(input_path: str, output_path: str, speed: float):
+def change_speed(audio, speed: float):
     """
     Change the speed of an audio file by a given percentage.
     """
-    # Load the audio file
-    audio, sample_rate = librosa.load(input_path)
-
-    # Change the speed
     changed_audio = librosa.effects.time_stretch(audio, rate=speed)
-
-    # Save the modified audio to a new file
-    sf.write(output_path, changed_audio, sample_rate)
+    return changed_audio
 
 
 # Function to shift pitch
-def shift_pitch(input_path: str, output_path: str, n_steps: int):
+def shift_pitch(audio, sampling_rate, n_steps: int):
     """
     Shift the pitch of an audio file by a given number of steps.
     """
-    y, sr = librosa.load(input_path)
-    y_shifted = librosa.effects.pitch_shift(y, sr=sr, n_steps=n_steps)
-    sf.write(output_path, y_shifted, sr)
+    audio_shifted = librosa.effects.pitch_shift(
+        audio, sr=sampling_rate, n_steps=n_steps
+    )
+    return audio_shifted
