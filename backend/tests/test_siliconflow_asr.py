2750cd433d9fbb9e9736ecaaca56725796bad4a0
mrm <2270929247@qq.com>
2025-02-19 17:36:12 +0800

    update pytest

--- backend/tests/test_siliconflow_asr.py
+++ backend/tests/test_siliconflow_asr.py
@@ -2,11 +2,14 @@ from threading import Event
 import os
 import sys
 
-sys.path.append('..')
 from audio_server_pipeline import ASRHandler
 
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 
-def main():
+
+
+def test_asr():
+
     stop_event = Event()
     interruption_event = Event()
     cur_conn_end_event = Event()
@@ -18,10 +21,10 @@ def main():
         interruption_event=interruption_event,
     )
 
-    file_path = "res/只用一个文.mp3"
+    file_path = os.path.join(PROJECT_ROOT, "backend/tests/res/只用一个文.mp3")
 
     response = model.process(file_path)
     print(response)
 
 if __name__ == "__main__":
-    main()
+    test_asr()
