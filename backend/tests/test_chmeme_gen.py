b4e88a98b4f638746bf4c191c9337c8174fed78e
wqj2004 <2285705435@qq.com>
2025-03-06 08:34:49 +0000

    add chmeme

---
+++ backend/tests/test_chmeme_gen.py
@@ -0,0 +1,268 @@
+import csv
+import base64
+import os
+from openai import OpenAI
+import random
+import json
+import re
+import numpy as np
+from PIL import Image
+# CSV file path
+#csv_file_path='/mnt/afs/xueyingyi/meme/data/label_C.csv'
+#csv_file_path = "/mnt/afs/niuyazhe/data/meme/data/label_C.csv"
+translation={
+    'sentiment category': '情感类别',
+    'happiness':'开心',
+    'love':'爱',
+    'anger':'愤怒',
+    'sorrow':'悲伤',
+    'fear':'恐惧',
+    'hate':'恨',
+    'surprise':'惊讶',
+    'sentiment degree':'情感程度', 
+    'slightly':'轻微',
+    'moderately':'中等',
+    'very':'强烈',
+    'intention detection':'情感意图',
+    'interactive':'交互',
+    'expressive':'表达',
+    'entertaining':'娱乐',
+    'offensive':'冒犯',
+    'other':'其他',
+    'offensiveness detection':'冒犯性检测',
+    'non-offensive':'非冒犯',
+}
+
+
+def get_tot_data(data_path):
+    # Create a dictionary to store the processed data
+    data_dict = {}
+
+    # Open the CSV file and read its contents
+    with open(data_path, mode='r', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+    
+        # Iterate over each row in the CSV
+        for row in csv_reader:
+            # Use 'images_name' as the key for the outer dictionary
+            image_name = row['images_name']
+            # Remove 'images_name' from the row to create the inner dictionary
+            row.pop('images_name')
+            # Store the inner dictionary in the outer dictionary
+            data_dict[image_name] = row
+    labels=['sentiment category','sentiment degree','intention detection','offensiveness detection']
+    # 遍历字典并修改内容,去掉括号,只保留括号内的文字
+    for image_name, details in data_dict.items():
+        for key in labels:
+            content=details[key]
+            details[key]=content[2:-1]
+    return data_dict
+
+    
+
+
+def get_smaples_random(data,sample_num=None):
+    data_dict=data
+    if sample_num is None:
+        sample_num=100
+    num_samples=sample_num
+    random_numbers=random.sample(range(0,len(data_dict)),num_samples)
+    return random_numbers
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        img_base64 = base64.b64encode(image_file.read()).decode('utf-8')
+    with Image.open(image_path) as img:
+        img_format=img.format.lower()
+    return img_base64,img_format
+
+
+def get_ans_chmeme_ch(data_dict,image_list,client,end_point,cared_keys,sys_prompt):
+    ch_dataset={}
+    error_imgs=[]
+    token_usage=0
+    for image_path in image_list:
+        # set the max number of tiles in `max_num`
+        print(f"processing {image_path}...")
+        base64_image,image_type = encode_image(image_path)
+        img_name=image_path.split('/')[-1]
+        # single-image single-round conversation (单图单轮对话)
+        #prompt_path='/mnt/afs/wangqijian/meme/ch_memes/prompt_gen_test_chmemes.txt'
+        #prompt_path='/mnt/afs/wangqijian/meme/ch_memes/prompt_gen_test_emodata_en.txt'
+        img_details=data_dict[img_name]
+        question='\n下面我将根据上面的要求分析这个表情包,并且按照样例的格式输出我的分析结果. 这个表情包的'
+        for element in cared_keys:
+            detail=f'{translation[element]} 是 {translation[img_details[element]]}, '
+            question+=detail
+        if img_details['metaphor occurrence']=='1':
+            add_prompt='注意在这个表情包中存在隐喻,其中'
+            src=img_details['source domain']
+            tgt=img_details['target domain']
+            src_parts = re.split(r'[;；]', src)
+            tgt_parts = re.split(r'[;；]', tgt)
+            for src_item,tgt_item in zip(src_parts,tgt_parts):
+                add_prompt+=f', \'{tgt_item}\' 有 \'{src_item}\' 的意思' 
+            question+=add_prompt
+        
+        try:
+            response = client.chat.completions.create(
+            # 替换为您的
+                model=end_point,
+                messages=[
+                    {
+                        "role": "system",
+                        "content":sys_prompt
+                    },
+                    {
+                      "role": "user",
+                      "content": [
+                        {
+                          "type": "text",
+                          "text": question,
+                        },
+                        {
+                          "type": "image_url",
+                          "image_url": {
+                            "url":  f"data:image/{image_type};base64,{base64_image}"
+                          },
+                        },
+                      ],
+                    }
+                        ],
+                )
+            ret_text=response.choices[0].message.content
+            token_usage+=response.usage.total_tokens
+            tmpdata={}
+            tmpdata['description']=ret_text
+            for key in cared_keys:
+                tmpdata[key]=img_details[key]
+            tmpdata['prompt']=question
+            ch_dataset[image_path]=tmpdata
+            print(f"processing {image_path} finished")
+        #except ZeroDivisionError as e:
+        except:
+            error_imgs.append(image_path)
+            print(f"Error processing image {image_path}")
+            #print(e)python
+            continue
+    print(f'You have used {token_usage} tokens for the dataset generation')
+    print(f'The number of error images:{len(error_imgs)}')
+    print(f'error imgs:')
+    for img in error_imgs:
+        print(img)
+    return ch_dataset
+
+def save_to_json(data, output_path):
+    with open(output_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+def get_dataset(data,prompt_path,client_endpoint,prefix,select_numbers=None):
+    #data_dict=get_tot_data(csv_file_path)
+    data_dict=data
+    if select_numbers is None:
+        random_numbers=get_smaples_random(data_dict,10)
+    else:
+        random_numbers=select_numbers
+
+    random_numbers.sort()
+    image_list=[]
+
+    #img_l=[656,1371,1558,1584,656,908,186,1037,4410,5554,6004,5976,5609,4299]
+    #can be modified 
+    img_l=random_numbers
+
+    for num in img_l:
+        img_path=f'{prefix}({num}).jpg'
+        image_list.append(img_path)
+    
+    client = OpenAI(
+        # 从环境变量中读取您的方舟API Key
+        api_key=api_key,
+        base_url=model_url,
+        #base_url="https://ark.cn-beijing.volces.com/api/v3",
+        )
+    cared_keys=['sentiment category','sentiment degree','intention detection','offensiveness detection']
+
+    with open(prompt_path, 'r',encoding='utf-8') as file:
+        PROMPT = file.read()
+    ch_dataset=get_ans_chmeme_ch(data_dict,image_list,client,client_endpoint,cared_keys,PROMPT)
+    
+    
+    #image_list=['/mnt/afs/niuyazhe/data/meme/data/Cimages/Cimages/Cimages/Image_(272).jpg']   
+    json_dataset={}
+    for num in img_l:
+        img_path=f'{prefix}({num}).jpg'
+        new_img_path=f'{num}'
+        cur_data={}
+        cur_details=ch_dataset[img_path]
+        text_details=cur_details['description']
+        lines=text_details.strip().split('\n')
+        for line in lines:
+            if line.startswith("(0)"):
+                cur_data["content"] = line[3:].strip()
+
+            elif line.startswith("(1)"):
+                cur_data["emotion"] = line[3:].strip()
+            
+            elif line.startswith("(2)"):
+                cur_data["intention"] = line[3:].strip()
+            
+            elif line.startswith("(3)"):
+                cur_data["scene"] = line[3:].strip()
+        json_dataset[new_img_path]=cur_data
+        
+    return json_dataset
+    
+    
+csv_file_path='/mnt/afs/wangqijian/meme/ch_memes/label_C.csv'
+#path to the image files
+image_prefix='/mnt/afs/niuyazhe/data/meme/data/Cimages/Cimages/Cimages/Image_'
+#path to the prompt
+prompt_path='/mnt/afs/wangqijian/meme/ch_memes/prompt_gen_test_chmemes_ch.txt'
+#path to save the dataset
+dataset_path_json='/mnt/afs/wangqijian/meme/ch_memes/dataset_4.json'
+#new_json_path='/mnt/afs/wangqijian/meme/ch_memes/test.json'
+#need to set API_KEY
+api_key=os.environ.get("ARK_API_KEY")
+#set model_url
+model_url="https://ark.cn-beijing.volces.com/api/v3"
+#first need to set model_endpoint in website
+model_endpoint="ep-20250124115350-pwjxt" 
+def main():
+    data=get_tot_data(csv_file_path)
+    processed_numbers=[
+    272, 4123, 3686, 497, 4997, 5405, 5695, 1879, 262, 1747,
+    1998, 1869, 2477, 654, 1261, 5335, 3862, 3498, 5629, 2968,
+    1873, 1185, 5677, 1765, 1217, 5253, 3787, 2489, 5645, 5567,
+    3715, 2209, 2622, 81, 1401, 1489, 835, 1515, 1328, 2862,
+    367, 3306, 671, 1095, 4732, 3097, 459, 5485, 6014, 427,
+    853, 82, 1410, 5705, 305, 4033, 989, 522, 462, 5452,
+    4147, 1098, 3999, 3240, 705, 1058, 1110, 1812, 4249, 4128,
+    2720, 735, 2972, 1864, 2370, 5380, 4488, 4730, 908, 3923,
+    4884, 5014, 3096, 2463, 3015, 4947, 931, 4984, 5200, 3626,
+    6012, 3315, 628, 3734, 708, 2805, 5022, 5552, 5765, 1072
+    ]
+    #tot_numbers=range(600,1800)
+    #tot_numbers=range(1800,4200)
+    tot_numbers=range(0,6046)
+    p_np=np.array(processed_numbers)
+    t_np=np.array(tot_numbers)
+    select_numbers=list(np.setdiff1d(t_np,p_np))
+    json_dataset=get_dataset(data,prompt_path,model_endpoint,image_prefix,select_numbers)
+    save_to_json(json_dataset,dataset_path_json)
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
---
+++ backend/tests/test_chmeme_rec.py
@@ -0,0 +1,95 @@
+import json
+import os
+import torch
+from transformers import AutoModel
+import requests
+from sklearn.metrics.pairwise import cosine_similarity
+from PIL import Image
+
+
+class ch_meme:
+    def __init__(self,data,embedding_model_name='online'):
+        self.data=data
+        self.embedding_model_name=embedding_model_name
+        # ids of chmeme
+        self.rec_nums=list(data.keys())
+        keys=list(data[self.rec_nums[0]].keys())
+        self.key_len=len(keys)
+        if embedding_model_name=='online':
+            self.embedding_model_url = os.getenv("EMBEDDING_URL")
+            if self.embedding_model_url is None:
+                self.embedding_model_url="http://103.177.28.193:30086/embedding"
+        elif embedding_model_name=='jina':
+            self.embedding_model = AutoModel.from_pretrained("jinaai/jina-embeddings-v3", trust_remote_code=True)
+        else:
+            raise KeyError(f'Incorrect arg: {self.embedding_model_name}')
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        if self.embedding_model_name != 'online':
+            self.embedding_model = self.embedding_model.to(device)
+    
+    def _get_embed_sim(self,query):
+        data_sentence=[]
+        for num in self.rec_nums:
+            item=self.data[num]
+            for key in list(item.keys()):
+                data_sentence.append(item[key])
+        if self.embedding_model_name == 'bert':
+            similarities = self.embedding_model.similarity(query, data_sentence)
+        else:
+            if self.embedding_model_name == 'online':  
+                payload = {
+                    "text": [query] + data_sentence,
+                    # "text": ['你好','你好好'],
+                    "model": "bge-large-zh-v1.5"
+                }
+                headers = {
+                    "Content-Type": "application/json"
+                }
+                response = requests.post(self.embedding_model_url, json=payload, headers=headers)
+                embeddings = response.json()['embedding']
+            else:
+                embeddings = self.embedding_model.encode([query] + data_sentence)
+            embeddings_query = torch.tensor(embeddings[0]).reshape(1, -1) 
+            embeddings_sentences = torch.tensor(embeddings[1:])            
+            similarities = cosine_similarity(embeddings_query, embeddings_sentences)
+        return similarities[0]
+    
+    def _get_sim_score(self,query):
+        sims=self._get_embed_sim(query)
+        sims=torch.from_numpy(sims)
+        key_len=self.key_len
+        sims=sims.view(-1,key_len).float()
+        scores=sims.mean(dim=1).tolist()
+        return scores
+                
+    
+    def get_topk_meme(self, query, k=2):
+        scores=self._get_sim_score(query)
+        sorted_with_index = sorted(enumerate(scores), key=lambda x: x[1], reverse=True)
+
+        top_k_indexes = [index for index, _ in sorted_with_index[:k]]
+
+        res = []
+        for ind in top_k_indexes:
+            res.append(self.rec_nums[ind])
+        return res
+    
+    
+    
+def test_rec_chmeme(query='耗子尾汁',k=4):
+    # json file path
+    file_path='/mnt/afs/wangqijian/meme/ch_memes/new_100_chmeme.json'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        ch_dataset = json.load(file)
+    # as for ch_meme, only support online embedding model
+    # other models can be added later
+    my_chmeme=ch_meme(data=ch_dataset)
+    return_id=my_chmeme.get_topk_meme(query,k)
+    prefix='/mnt/afs/niuyazhe/data/meme/data/Cimages/Cimages/Cimages/Image_'
+    image_path=[f'{prefix}({x}).jpg' for x in return_id]
+    for path in image_path:
+        print(path)
+        img=Image.open(path)
+        img.show()
+
+test_rec_chmeme()
